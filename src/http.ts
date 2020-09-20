2399e2fb282b450b8454fba7ec6d08f7d3af9832
Max <max.netterberg@gmail.com>
2020-09-20 19:42:28 +0200

    inital commit

---
+++ src/http.ts
@@ -0,0 +1,46 @@
+export type HttpConfig = {
+  baseUrl?: string;
+};
+
+export interface HttpResponse extends Response {
+  data: any;
+}
+
+export class Http {
+  private baseUrl: string;
+
+  private constructor() {}
+
+  public static init({ baseUrl }: HttpConfig) {
+    const http = new Http();
+    http.baseUrl = baseUrl;
+    return http;
+  }
+
+  public get(endpoint: string, headers?: any) {
+    return this.baseRequest("GET", endpoint, headers);
+  }
+  public post(endpoint: string, headers?: any, body?: any) {
+    return this.baseRequest("POST", endpoint, headers, body);
+  }
+
+  private baseRequest(
+    method: string,
+    endpoint: string,
+    headers?: any,
+    body?: any
+  ) {
+    return new Promise<HttpResponse>(async (resolve) => {
+      const res = await fetch(`${this.baseUrl}/${endpoint}`, {
+        method,
+        headers,
+        body: JSON.stringify(body),
+      });
+      const response: HttpResponse = {
+        ...res,
+        data: JSON.stringify(res.body),
+      };
+      resolve(response);
+    });
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import { Http } from "./http";
+
+export type KeyHoleOptions = {
+  apiBaseUrl: string;
+  autoSync?: boolean;
+  syncTime?: number;
+};
+
+export type Credentials = {
+  email: string;
+  password: string;
+};
+
+export type KeyHoleTokens = {
+  token: string;
+  refreshToken: string;
+};
+
+export type KeyHoleUserInfo = {
+  email: string;
+  id: number;
+};
+
+export class KeyHole {
+  private interval: NodeJS.Timeout;
+  private options: KeyHoleOptions;
+  private tokens: KeyHoleTokens;
+  private http: Http;
+
+  public userInfo: KeyHoleUserInfo;
+
+  private constructor() {}
+
+  public static initialize(options: KeyHoleOptions) {
+    const keyhole = new KeyHole();
+    keyhole.options = options;
+    keyhole.http = Http.init({ baseUrl: options.apiBaseUrl });
+
+    if (options.autoSync) keyhole.startSync();
+  }
+
+  public async login(credentials: Credentials) {
+    const res = await this.http.post("/login", {}, credentials);
+
+    this.tokens = {
+      token: res.data.token,
+      refreshToken: res.data.refreshToken,
+    };
+    this.userInfo = res.data.user;
+  }
+
+  public async logout() {
+    const res = await this.http.post(
+      "/logout",
+      {},
+      { token: this.tokens.token }
+    );
+    return res.data;
+  }
+
+  public async refreshToken() {
+    const res = await this.http.post("/token/refresh", {}, this.tokens);
+    return res.data;
+  }
+
+  public async register(credentials: Credentials) {
+    const res = await this.http.post("/register", {}, credentials);
+    this.tokens = {
+      token: res.data.token,
+      refreshToken: res.data.refreshToken,
+    };
+    this.userInfo = res.data.user;
+  }
+
+  public stopSync() {
+    clearInterval(this.interval);
+  }
+
+  public startSync() {
+    this.interval = setInterval(() => {
+      this.refreshToken();
+    }, this.options.syncTime ?? 3300000); // Default to every 55th minute of token refresh
+  }
+}
